1895291547b49e1989bf5ae9a612b93d1d651fb4
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-04-05 20:11:31 +0530

    outbound rule and instance added

--- VPC/main.tf
+++ VPC/main.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "public-subnet-1" {
     vpc_id = aws_vpc.project_vpc.id
     cidr_block = var.public_sb1_cidr
     availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
     tags = {
         Name = "public-subnet-1"
     }
@@ -29,6 +30,7 @@ resource "aws_subnet" "public-subnet-2" {
     vpc_id = aws_vpc.project_vpc.id
     cidr_block = var.public_sb2_cidr
     availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
     tags = {
         Name = "public-subnet-2"
     }
@@ -106,4 +108,65 @@ resource "aws_route_table_association" "rta_to_private1" {
 resource "aws_route_table_association" "rta_to_private2" {
     subnet_id = aws_subnet.private-subnet-2.id
     route_table_id = aws_route_table.private_route_table.id
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "rsa" {
+    algorithm = "RSA"
+    rsa_bits  = 4096
+}
+
+# https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+# Create the AWS key pair
+resource "aws_key_pair" "TF_key" {
+    key_name   = "TF-key"
+    public_key = tls_private_key.rsa.public_key_openssh
+}
+
+# https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
+# Save the private key to a local file
+resource "local_file" "TF_key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "tfkey"
+}
+
+# Security Group
+resource "aws_security_group" "TF_SG" {
+    name        = "security_group_using_terraform"
+    description = "security group using terraform"
+    vpc_id      = aws_vpc.project_vpc.id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "allow SSH"
+        }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "allow HTTP"
+        }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
+}
+
+# Launch an EC2 instance using the created key pair
+resource "aws_instance" "web" {
+    ami                    = var.ami_id  # Specify an appropriate AMI ID
+    instance_type          = var.instance_type
+    key_name               = aws_key_pair.TF_key.key_name  # Reference the key_name from aws_key_pair resource
+    vpc_security_group_ids = [aws_security_group.TF_SG.id]
+    subnet_id              = aws_subnet.public-subnet-1.id  # Use subnet ID instead of subnet name
+    user_data              = filebase64("./example.sh")
 }
