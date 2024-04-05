6f02a0f54956cdaa4bd3fcbe392ff845d505ce41
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-04-05 20:15:42 +0530

    Add files via upload

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,5 +1,5 @@
 variable "ami_id" {
-    default = "your_ami_id"
+    default = "ami-07d9b9ddc6cd8dd30"
 }
 
 variable "instance_type" {
@@ -7,5 +7,5 @@ variable "instance_type" {
 }
 
 variable "vpc_id" {
-    default = "your_vpc_id"
+    default = "vpc-0d302a54a6ebd6d97"
 }
